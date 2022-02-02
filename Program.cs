93462d4914beb7be2f0119eb0e35e80bf48efa8b
roman11235 <98908468+roman11235@users.noreply.github.com>
2022-02-02 20:22:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Please enter the correct math expression in one line and press Enter.");
+                Console.WriteLine("=" + Convert.ToDecimal(new DataTable().Compute(Console.ReadLine(), null)));
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect math expression");
+            }
+
+            Console.WriteLine("press Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Calc
+# calc
+Compile the program, run.
+Enter the correct mathematical expression in one line and press Enter..
+Use the decimal separator in numbers if necessary.
+Enjoy :)
+
+Use UAT if necessary.
